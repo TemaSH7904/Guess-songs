44dcec141191ce581b1aed1e07c63a6060405e98
ArtemSh01 <artemschalaeff@yandex.ru>
2022-10-28 22:49:22 +0300

    version test_get_songs and test_get_variants 1.0

--- test.py
+++ test.py
@@ -10,4 +10,15 @@ class TestGame(unittest.TestCase):
     def test_start_game(self):
          result = main.start_game()
          self.assertEqual(result, "Good")
+    def test_get_random_song(self):
+          result = main.get_random_song()
+          self.assertEqual(result, "0 song -- CC CATCh - Cause You Are Young.mp3")
+
+    def test_get_songs(self):
+         result = main.get_songs()
+         self.assertEqual(result, ["1 song Basta-SolnceNeVidno.mp3"])
+
+    def test_get_variants(self):
+         result = main.get_var()
+         self.assertEqual(result, "2 song Basta-yrban.mp3")
 
