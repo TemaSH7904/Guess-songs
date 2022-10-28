ddbeb869f47cc52f0d2f892d56fd1985451cc1c3
ArtemSh01 <artemschalaeff@yandex.ru>
2022-10-28 22:48:05 +0300

    version test_start_game 1.0

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ class TestGame(unittest.TestCase):
     def test_menu(self):
         result = main.menu()
         self.assertEqual(result, "0")
+
+    def test_start_game(self):
+         result = main.start_game()
+         self.assertEqual(result, "Good")
+
